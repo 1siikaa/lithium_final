2a8a56c91bd6d85cd66a4656174ed22e2ebd2611
1siikaa <srishtidwivedi672@gmail.com>
2022-10-13 09:40:19 +0530

    pushing my branch

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Lithium
 Backend cohort Oct 2022 - Jan 2023
-
+ 
+ // hi, this is Vanshika
+ 
 
 # Assignment
 // Will add this later
--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+// i am currently enrolled with functionup
