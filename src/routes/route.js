fee1b460c7c54d2ccabd1852f02b7976cceb99d9
1siikaa <srishtidwivedi672@gmail.com>
2022-10-17 19:36:46 +0530

    assignment solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -38,6 +38,71 @@ router.get('/student-details/:name', function(req, res){
     let studentName = requestParams.name
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
-})
+});
 
+// Problem no. 1 ==>
+router.get('/movies', function(req, res){
+let list = ["Rand de basanti","The shining","Lord of the rings", "Batman begins"]
+console.log("The path params in the request are : ", req.params)
+console.log("The list of movies are : ", list)
+res.send(list)
+});
+
+// Problem no. 2 & 3 ==>
+router.get('/movies/:indexNumber', function(req, res){
+    let list = ["Rand de basanti","The shining","Lord of the rings", "Batman begins"]
+   const index = req.params.indexNumber
+   if(index <= list.length-1){
+    console.log("The movie at this index is : ", list[index])
+  res.send(list[index])
+  }
+   else{
+    res.send("error message: Please use a valid index.")
+   }
+  
+    }); 
+
+
+// Problem no. 4 ==>
+router.get('/films', function(req, res){
+    
+        films = [ {
+            id: 1,
+            name: "The Shining"
+           }, {
+            id: 2,
+            name: "Incendies"
+           }, {
+            id: 3,
+            name: "Rang de Basanti"
+           }, {
+            id: 4,
+            name: "Finding Nemo"
+           }]
+           
+        res.send(films)  
+        }) 
+
+        
+// Problem no. 5 ==>
+router.get('/films/:filmId', function(req, res){
+    films = [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+const reqFilm = films.find(element=>element.id==req.params.filmId)
+if(reqFilm == undefined){
+    res.send('No movie exists with this id')
+}
+res.send(reqFilm)
+})
 module.exports = router;
