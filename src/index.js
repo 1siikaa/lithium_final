22738875a4616d297a79d04783166d946b4b6699
1siikaa <srishtidwivedi672@gmail.com>
2022-10-21 16:03:26 +0530

    database

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb+srv://Vanshika:1siikaaon7*@cluster0.9talhbs.mongodb.net/Vanshika-07032001", {useNewUrlParser:true})
+.then(()=>console.log('mongoDB is connected successfully'))
+.catch(err=>console.log(err))
 
 app.use('/', route);
 
