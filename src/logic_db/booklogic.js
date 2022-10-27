9b801bdbbdeaee3532cdbba38e7f5c4958fa6589
1siikaa <srishtidwivedi672@gmail.com>
2022-10-27 12:25:48 +0530

    done

--- src/logic_db/booklogic.js
+++ src/logic_db/booklogic.js
@@ -31,7 +31,7 @@ const getParticularBooks = async function (req, res){
 
 const getXINRBooks = async function (req, res){
     let getpricedata = await BookModel.find({
-        indianPrice:{$in:['100INR', '200INR', '500INR']}
+        "price.indianPrice":{$in:['100INR', '200INR', '500INR']}
     })
     res.send({msg: getpricedata})
 }
